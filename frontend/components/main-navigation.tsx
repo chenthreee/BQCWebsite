b6c815d34b3a3782ae0dee62ca57c08bc333b1a5
chenthreee <1160020938@qq.com>
2025-12-05 09:33:24 +0800

    关于我们-公司介绍部分 增加小段标题

--- frontend/components/main-navigation.tsx
+++ frontend/components/main-navigation.tsx
@@ -238,6 +238,7 @@ export function MainNavigation() {
             { label: mounted ? t("大型船舶储能BMS") : "大型船舶储能BMS", href: `/${currentLocale}/products/energy-storage-bms/large-ship.html` },
             { label: mounted ? t("小型船舶储能BMS") : "小型船舶储能BMS", href: `/${currentLocale}/products/energy-storage-bms/small-ship.html` },
             { label: mounted ? t("通讯基站储能BMS") : "通讯基站储能BMS", href: `/${currentLocale}/products/energy-storage-bms/communication-base.html` },
+            //这里暂时删除了储能BMS下面 EMS总控这个分类 但是不删代码 万一以后又觉得要加回来就麻烦了……
             //{ label: mounted ? t("储能EMS（总控）") : "储能EMS（总控）", href: `/${currentLocale}/products/energy-storage-bms/energy-storage-ems.html` },
             { label: mounted ? t("储能PCS") : "储能PCS", href: `/${currentLocale}/products/energy-storage-pcs.html` },
           ],
