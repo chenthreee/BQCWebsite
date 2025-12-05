fe5cdf17c0b9cd57111857013cb9110650b15348
chenthreee <1160020938@qq.com>
2025-12-05 15:39:24 +0800

    修改网站标签加入bqc图标

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -34,7 +34,8 @@ export default function RootLayout({
   return (
     <html lang="zh-CN" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+        {/*<link rel="icon" href="/favicon.ico" sizes="any" />*/}
+        <link rel="icon" href="/BQCLogo.png" sizes="any" />
       </head>
       <body className={inter.className}>
         <LanguageProvider>
